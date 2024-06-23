25bb14df6a48f3fd3b8186eff8f7e90318d0f70b
Harshill Aggarwal <harshillsms@gmail.com>
2024-06-23 20:45:18 +0530

    update debug 4

--- src/debug4.cpp
+++ src/debug4.cpp
@@ -42,21 +42,54 @@ using namespace std;
 // }
 
 // find min in rotated sorted array
-int findMin(vector<int>& nums) {
-    int start = 0, end = nums.size()-1, ans = start;
-    while(nums[start] > nums[end]) {
-        int mid = (start + end) >> 1;
-        if(nums[mid] <= nums[start] && nums[mid] <= nums[end]) 
-            end = mid;
-        else if(nums[mid] >= nums[start] && nums[mid] >= nums[end])
-            start = mid+1;          
-        else return nums[mid];
-        ans = start;
-    }
-    return nums[ans];
-}
+// int findMin(vector<int>& nums) {
+//     int start = 0, end = nums.size()-1, ans = start;
+//     while(nums[start] > nums[end]) {
+//         int mid = (start + end) >> 1;
+//         if(nums[mid] <= nums[start] && nums[mid] <= nums[end]) 
+//             end = mid;
+//         else if(nums[mid] >= nums[start] && nums[mid] >= nums[end])
+//             start = mid+1;          
+//         else return nums[mid];
+//         ans = start;
+//     }
+//     return nums[ans];
+// }
 
-int main() {
+// find peak element
+// int findPeakElement(vector<int>& nums) {
+//     int n = nums.size();
+//     int low = 0;
+//     int high = n-1;
+//     while(low < high){
+//         int mid = (low + high) >> 1;
+//         if(nums[mid] > nums[mid+1]){
+//             high = mid;
+//         }
+//         else{
+//             low = mid+1;
+//         }
+//     }
+//     return nums[low];
+// }
+
+// longest strictly increasing subsequence
+// int lengthOfLIS(vector<int>& nums){
+//     vector<int> ans;
+//     ans.push_back(nums[0]);
+//     for(int i=0; i<nums.size(); i++){
+//         if(nums[i]>ans.back()){
+//             ans.push_back(nums[i]);
+//         }
+//         else{
+//             int index=lower_bound(ans.begin(), ans.end(), nums[i])-ans.begin(); // Before solving get acquainted with lower_bound()
+//             ans[index]=nums[i];
+//         }
+//     }
+//     return ans.size();
+// }
+
+int main(){
 
     // vector<int> nums = {5};
     // int target = 5;
@@ -65,8 +98,13 @@ int main() {
     // int x = 30;
     // cout << mySqrt(x);
 
-    vector<int> nums = {3,4,5,1,2};
-    cout << findMin(nums);
+    // vector<int> nums = {3,4,5,1,2};
+    // cout << findMin(nums);
+
+    // vector<int> nums = {1,2,3};
+    // cout << findPeakElement(nums);
+
+
 
     return 0;
 }
