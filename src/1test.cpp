e6b8692b17b867313fb7e3afde0cf0bd4972d78c
Harshill Aggarwal <harshillsms@gmail.com>
2024-07-07 18:07:31 +0530

    testing 2d pointers

--- src/1test.cpp
+++ src/1test.cpp
@@ -20,6 +20,16 @@ int main() {
     // int size = 6;
     // printAllPairs(arr,size);
 
+    int x[3][5] = {
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15}
+    };
+
+    int *n = &x[0][0];
+
+    cout<< *(n+5)+1;
+
     return 0;
 
 }
